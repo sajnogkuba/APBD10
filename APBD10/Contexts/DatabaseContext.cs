54c00587e42bda68d9b748b5c75aaeb2c72b017a
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-29 09:07:37 +0200

    add Product model and Products table to database

--- APBD10/Contexts/DatabaseContext.cs
+++ APBD10/Contexts/DatabaseContext.cs
@@ -1,14 +1,21 @@
+using APBD10.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace APBD10.Contexts;
 
 public class DatabaseContext : DbContext
 {
+    public DbSet<Product> Products { get; set; }
     protected DatabaseContext()
     {
     }
 
     public DatabaseContext(DbContextOptions options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
---
+++ APBD10/Models/Product.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APBD10.Models;
+
+[Table("Products")]
+public class Product
+{
+    [Key]
+    [Column("PK_Product")]
+    public int ProductId { get; set; }
+    
+    [Column("name")]
+    [MaxLength(100)]
+    public string ProductName { get; set; }
+    
+    [Column("width", TypeName = "decimal(5,2)")]
+    public decimal ProductWidth { get; set; }
+    
+    [Column("height", TypeName = "decimal(5,2)")]
+    public decimal ProductHeight { get; set; }
+    
+    [Column("depth", TypeName = "decimal(5,2)")]
+    public decimal ProductDepth { get; set; }
+}
